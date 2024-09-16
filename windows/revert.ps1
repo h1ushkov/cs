9add8e0120c4c766c17224e48f092c28ffb6effa
h1ushkov <60152947+h1ushkov@users.noreply.github.com>
2024-09-16 09:26:50 +0300

    Update revert.ps1

--- windows/revert.ps1
+++ windows/revert.ps1
@@ -1,4 +1,4 @@
-﻿# Проверка наличия прав администратора
+# Проверка наличия прав администратора
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Host "This script requires administrator privileges." -ForegroundColor Red
     exit
